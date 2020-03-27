8a9e15dfa238fe5ca4eb3865a79e65bb7d3504a5
sourcefranke <lukas.tietenberg@outlook.de>
2020-03-27 15:43:39 +0100

    First Draft for unit tests directly in Kotlin

--- src/main/kotlin/com/github/sourcefranke/rpgdicearith/RpgDiceArithmetics.kt
+++ src/main/kotlin/com/github/sourcefranke/rpgdicearith/RpgDiceArithmetics.kt
@@ -3,35 +3,63 @@ package com.github.sourcefranke.rpgdicearith
 import kotlin.random.Random
 
 fun rollDiceResult(formula: String): Int {
-	return formula
-		.filter{c -> !c.isWhitespace()}
-		.split("+")
-		.flatMap{string -> getElement(string)}
-		.sumBy { e -> e.getResult() }
+	return getElement(shorten(formula)).getResult()
 }
 
-fun getElement(subString: String): List<Element> {
+fun shorten(string: String): String {
+	return string.replace("\\s".toRegex(), "")
+}
+
+fun getElement(string: String): Element {
+	var element: Element
 	when {
-		subString.matches(Regex("[d][0-9]+"))		-> return listOf(Dice(subString.substring(1)))
-		subString.matches(Regex("[0-9]+"))			-> return listOf(Number(subString))
-		subString.matches(Regex("[0-9]+[d][0-9]+"))	-> return getMultipleDices(subString)
-		else										-> throw Exception()
+		isAddition(string) 		-> element = createAddition(string)
+		isSingleDice(string)	-> element = Dice(string.substring(1))
+		isSingleNumber(string)	-> element = Number(string)
+		isMultipleDices(string)	-> element = getMultipleDices(string)
+		else					-> throw Exception()
 	}
+	return element
 }
 
-fun getMultipleDices(string: String): List<Element> {
+fun isAddition(string: String): Boolean {
+	return string.matches(Regex(".*\\+.*"))
+}
+
+fun createAddition(string: String): Element {
+	var list = string.split("+")
+	return Addition(getElement(list.get(0)), getElement(list.get(1)))
+}
+
+fun isSingleDice(string: String): Boolean {
+	return string.matches(Regex("[d][0-9]+"))
+}
+
+fun isSingleNumber(string: String): Boolean {
+	return string.matches(Regex("[0-9]+"))
+}
+
+fun isMultipleDices(string: String): Boolean {
+	return string.matches(Regex("[0-9]+[d][0-9]+"))
+}
+
+fun getMultipleDices(string: String): Element {
 	var list = string.split("d")
-	
-	if(list.size != 2) throw Exception()
-	
-	var amount = list.get(0).toInt()
-	var diceType = list.get(1).toInt()
-	
-	return generateSequence { Dice(diceType) }.take(amount).toList()
+
+	var repeats = list.get(0).toInt() - 1
+	var diceMax = list.get(1).toInt()
+
+	var element = Addition(Number(0), Dice(diceMax))
+	repeat(repeats) {
+		element = Addition(element, Dice(diceMax))
+	}
+	return element
 }
 
 interface Element {
 	fun getResult(): Int
+	fun getMin(): Int
+	fun getMax(): Int
 }
 
 class Number (val number: Int): Element {
@@ -41,13 +69,59 @@ class Number (val number: Int): Element {
 	override fun getResult(): Int {
 		return number
 	}
+	
+	override fun getMin(): Int {
+		return number
+	}
+	
+	override fun getMax(): Int {
+		return number
+	}
 }
 
-class Dice (val max: Int): Element {
-	
+class Dice (val number: Int): Element {
+
 	constructor (maxString: String) : this(maxString.toInt())
 	
 	override fun getResult(): Int {
-		return Random.nextInt(max) + 1
+		return Random.nextInt(number) + 1
+	}
+	
+	override fun getMin(): Int {
+		return 1
+	}
+	
+	override fun getMax(): Int {
+		return number
+	}
+}
+
+class Addition (val first: Element, val second: Element): Element {
+	
+	override fun getResult(): Int {
+		return first.getResult() + second.getResult()
+	}
+	
+	override fun getMin(): Int {
+		return first.getMin() + second.getMin()
+	}
+	
+	override fun getMax(): Int {
+		return first.getMax() + second.getMax()
+	}
+}
+
+class Multiplication (val first: Element, val second: Element): Element {
+	
+	override fun getResult(): Int {
+		return first.getResult() * second.getResult()
+	}
+	
+	override fun getMin(): Int {
+		return first.getMin() * second.getMin()
+	}
+	
+	override fun getMax(): Int {
+		return first.getMax() * second.getMax()
 	}
 }
---
+++ src/test/kotlin/com/github/sourcefranke/rpgdicearith/RpgDiceArithmeticsTest.kt
@@ -0,0 +1,36 @@
+package com.github.sourcefranke.rpgdicearith
+
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.*
+
+class RpgDiceArithmeticsTest {
+
+	@Test
+	fun `"rollDiceResult" returns the final result for a dice formula`() {
+		assertThat(rollDiceResult("7")).isEqualTo(7)
+		assertThat(rollDiceResult("4 + 7")).isEqualTo(11)
+		assertThat(rollDiceResult("4d2")).isGreaterThanOrEqualTo(4).isLessThanOrEqualTo(8)
+	}
+
+	@Test
+	fun `"shorten" eleminates all whitespace characters`() {
+		assertThat(shorten(" d   6")).isEqualTo("d6")
+		assertThat(shorten("2 d6  + 5 ")).isEqualTo("2d6+5")
+		assertThat(shorten(" 2	d6")).isEqualTo("2d6")
+	}
+
+	@Test
+	fun `"isSingleDice" returns True for a dice`() {
+		assertThat(isSingleDice("d6")).isTrue()
+	}
+
+	@Test
+	fun `"isSingleDice" returns False for everything else`() {
+		assertThat(isSingleDice("6")).isFalse()
+		assertThat(isSingleDice("3d6")).isFalse()
+		assertThat(isSingleDice("3+d6")).isFalse()
+		assertThat(isSingleDice("d6+3")).isFalse()
+		assertThat(isSingleDice("d 6")).isFalse()
+		assertThat(isSingleDice("d")).isFalse()
+	}
+}
